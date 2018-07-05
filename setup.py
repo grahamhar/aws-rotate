87ef6f864580721c02733ca1e0952326a64cf7c8
grahamhar <grahamhar@users.noreply.github.com>
2018-07-05 21:46:55 +0100

    Create a release to update readme in pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='aws-rotate',  # Required
 
-    version='0.0.2',  # Required
+    version='0.0.3',  # Required
 
     description='A simple AWS Access ID/Secret rotation script',  # Required
 
