62442b4ab2e464482cf61e698f3abccdd6d36817
grahamhar <grahamhar@users.noreply.github.com>
2018-07-08 18:45:03 +0100

    Increase test coverage

---
+++ tests/test_profile_set.py
@@ -0,0 +1,17 @@
+import os
+from aws_rotate import get_current_aws_profile
+
+
+def test_with_no_env_var():
+    """
+    With no environment variable set it should return default
+    """
+    assert get_current_aws_profile() == 'default'
+
+
+def test_with_env_var():
+    """
+    With environment variable set it should return the value of the env var
+    """
+    os.environ['AWS_PROFILE'] = 'test_profile_for_pytest'
+    assert get_current_aws_profile() == 'test_profile_for_pytest'
